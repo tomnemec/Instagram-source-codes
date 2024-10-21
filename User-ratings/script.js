67b214ffa8ee308f3865d21a59f4747655c8fccc
tomnemec <codin.with.tom@gmail.com>
2024-10-21 09:26:26 +0200

    init hover

--- User-ratings/script.js
+++ User-ratings/script.js
@@ -3,28 +3,26 @@ const weekFilter = document.getElementById("filter-week");
 const monthFilter = document.getElementById("filter-month");
 const container = document.getElementById("users-container");
 
-// Helper function to remove active class from all buttons
 function removeActiveClasses() {
   todayFilter.classList.remove("active");
   weekFilter.classList.remove("active");
   monthFilter.classList.remove("active");
 }
 
-// Add event listeners for filter items
 todayFilter.addEventListener("click", () => {
   removeActiveClasses();
-  todayFilter.classList.add("active"); // Add active to button
-  container.style.transform = "translateX(410px)"; // Move container for daily view
+  todayFilter.classList.add("active"); 
+  container.style.transform = "translateX(410px)";
 });
 
 weekFilter.addEventListener("click", () => {
   removeActiveClasses();
-  weekFilter.classList.add("active"); // Add active to button
-  container.style.transform = "translateX(10px)"; // Move container for weekly view
+  weekFilter.classList.add("active"); 
+  container.style.transform = "translateX(10px)"; 
 });
 
 monthFilter.addEventListener("click", () => {
   removeActiveClasses();
-  monthFilter.classList.add("active"); // Add active to button
-  container.style.transform = "translateX(-400px)"; // Move container for monthly view
+  monthFilter.classList.add("active"); 
+  container.style.transform = "translateX(-400px)"; 
 });
